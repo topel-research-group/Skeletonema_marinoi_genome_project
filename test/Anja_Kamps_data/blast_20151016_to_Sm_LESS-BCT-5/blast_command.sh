56ee5bca4c0a764c6c8fc156065e0b08603352fd
mtop <mats@topel.se>
2015-11-03 22:50:22 +0100

    Only saving one alignment in order to easier parse the output

--- test/Anja_Kamps_data/blast_20151016_to_Sm_LESS-BCT-5/blast_command.sh
+++ test/Anja_Kamps_data/blast_20151016_to_Sm_LESS-BCT-5/blast_command.sh
@@ -5,4 +5,4 @@
 #$ -pe mpich 60
 #$ -S /bin/bash
 
-blastn -query /nobackup/data5/Skeletonema_marinoi_genome_project/test/Anja_Kamps_data/20151016/Skeletonema_MiSeq_20151016_novo_LENGTH-SORTED.fa -db /nobackup/data5/data/skeletonema/assemblies/Sm_LESS-BCT-5_clc-assembly_novo.fa -num_alignments 10 -num_threads $NSLOTS -out Skeletonema_MiSeq_20151016_LONGEST_to_nr.BLASTn.txt
+blastn -query /nobackup/data5/Skeletonema_marinoi_genome_project/test/Anja_Kamps_data/20151016/Skeletonema_MiSeq_20151016_novo_LENGTH-SORTED.fa -db /nobackup/data5/data/skeletonema/assemblies/Sm_LESS-BCT-5_clc-assembly_novo.fa -num_alignments 1 -num_threads $NSLOTS -out Skeletonema_MiSeq_20151016_LONGEST_to_nr.ONE-MATCH.BLASTn.txt
